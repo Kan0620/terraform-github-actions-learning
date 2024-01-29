53d37298944a7be4770cd4f78e376bf05385f40b
Kan0620 <kantasoworld0620@gmail.com>
2024-01-29 23:55:34 +0900

    add: fastapi server

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,18 +1,18 @@
 resource "aws_instance" "public_server" {
-  ami           = "ami-0404778e217f54308"
-  instance_type = "t3.micro"
+    ami           = "ami-0404778e217f54308"
+    instance_type = "t3.micro"
 
-  # iam_instance_profile = aws_iam_instance_profile.demo.name
+    # iam_instance_profile = aws_iam_instance_profile.demo.name
 
-  vpc_security_group_ids = [
-    aws_security_group.security_group.id
-  ]
-  subnet_id = aws_subnet.public.id
-  # key_name = aws_key_pair.key_pair.id
-  user_data = file("./userdata.sh")
-  tags = {
-    Name = var.tag_name
-  }
+    vpc_security_group_ids = [
+        aws_security_group.security_group.id
+    ]
+    subnet_id = aws_subnet.public.id
+    # key_name = aws_key_pair.key_pair.id
+    user_data = file("./userdata.sh")
+    tags = {
+        Name = var.tag_name
+    }
 }
 
 # resource "aws_key_pair" "key_pair" {
@@ -22,5 +22,5 @@ resource "aws_instance" "public_server" {
 
 
 output "public_ip" {
-  value = aws_instance.public_server.public_ip
+    value = aws_instance.public_server.public_ip
 }
--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo yum install python3 -y
 pip3 install fastapi uvicorn
-sudo yum install git
-git clone git@github.com:Kan0620/terraform-github-actions-learning.git app
+sudo yum install git -y
+git clone https://gist.github.com/3564244597965cf4db54f41f511136ac.git
+cd 3564244597965cf4db54f41f511136ac
 python3 main.py
